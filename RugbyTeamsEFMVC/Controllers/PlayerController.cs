5cb196a29f015c7ca43fc09655b4bfb7cdf534f7
Spencer Lott <spklott@gmail.com>
2023-12-29 12:14:21 -0500

    completed create for player

--- RugbyTeamsEFMVC/Controllers/PlayerController.cs
+++ RugbyTeamsEFMVC/Controllers/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RugbyTeamsEFMVC.Models;
 using RugbyTeamsEFMVC.Repositories;
+using RugbyTeamsEFMVC.ViewModels;
 
 namespace RugbyTeamsEFMVC.Controllers
 {
@@ -16,6 +18,63 @@ namespace RugbyTeamsEFMVC.Controllers
         {
             IEnumerable<Player> players = _playerRepository.GetAllPlayers();
             return View(players);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePlayer()
+        {
+            var teams = _playerRepository.GetAllTeams();
+            ViewBag.Teams = new SelectList(teams, "Id", "Name");
+            List<string> positions = new List<string>()
+            {
+                "Loose-head prop",
+                "Hooker",
+                "Tight-head prop",
+                "Second row",
+                "Blind-side Flanker",
+                "Open-side Flanker",
+                "Number 8",
+                "Scrum-half",
+                "Fly-half",
+                "Left wing",
+                "Inside center",
+                "Outside center",
+                "Right wing",
+                "Full back"
+            };
+            ViewBag.Positions = new SelectList(positions);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePlayer(PlayerViewModel player) 
+        { 
+            if (!ModelState.IsValid) 
+            {
+                var teams = _playerRepository.GetAllTeams();
+                ViewBag.Teams = new SelectList(teams, "Id", "Name");
+                List<string> positions = new List<string>()
+            {
+                "Loose-head prop",
+                "Hooker",
+                "Tight-head prop",
+                "Second row",
+                "Blind-side Flanker",
+                "Open-side Flanker",
+                "Number 8",
+                "Scrum-half",
+                "Fly-half",
+                "Left wing",
+                "Inside center",
+                "Outside center",
+                "Right wing",
+                "Full back"
+            };
+                ViewBag.Positions = new SelectList(positions);
+                return View(player);
+            }
+            _playerRepository.AddPlayer(player);
+            return RedirectToAction("Index");
         }
     }
 }
--- RugbyTeamsEFMVC/ViewModels/PlayerViewModel.cs
+++ RugbyTeamsEFMVC/ViewModels/PlayerViewModel.cs
@@ -1,6 +1,69 @@
-﻿namespace RugbyTeamsEFMVC.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using RugbyTeamsEFMVC.Models;
+
+namespace RugbyTeamsEFMVC.ViewModels
 {
     public class PlayerViewModel
     {
+        public int Id { get; set; }
+
+        public PlayerViewModel() 
+        {
+            teams = new List<Team>();
+            positions = new List<string>();
+        }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z_]*$", ErrorMessage = "Please enter text")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z_]*$", ErrorMessage = "Please enter text")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        
+        public string? Notes { get; set; }
+        public string? Position { get; set; }
+
+        [Required(ErrorMessage = "Height in feet is required")]
+        [DisplayName("Height ft.")]
+        public int HeightFt { get; set; }
+
+        [Required(ErrorMessage = "Inches are required, if zero input \"0\"")]
+        [Range(0, 11, ErrorMessage = "The field for Inches must be between 0 and 11.")]
+        [DisplayName("Height in.")]
+        public int HeightIn { get; set; }
+
+        [Required(ErrorMessage = "Weight in pounds is required")]
+        public int Weight { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+
+        }
+
+        public string CompleteHeight
+        {
+            get
+            {
+                return $"{HeightFt}' {HeightIn}\"";
+            }
+        }
+
+        [Required(ErrorMessage = "Please make sure to select a team")]
+        public int TeamId { get; set; } //Foreign Key
+
+        public List<Team> teams { get; set; }
+        public List<string> positions { get; set; }
+
     }
 }
